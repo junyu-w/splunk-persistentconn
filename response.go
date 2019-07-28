ac56b1c0746749522c61054f36755577b05b26b7
Junyu Wang <junyuw@berkeley.edu>
2019-07-28 02:28:29 -0700

    fixed response packet protocol issue

--- response.go
+++ response.go
@@ -12,11 +12,13 @@ type Response struct {
 }
 
 // getRawData transforms the response to a payload that splunkd can decode
+// splunkd response protocol: 0\n<len_response_bytes>\n<response>
+// NOTE: leading 0 in protocol is necessary to let splunkd know it's a response
 func (resp Response) getRawData() string {
 	respData, err := json.Marshal(&resp)
 	if err != nil {
 		respData = []byte("Failed to serialize response data")
 	}
-	rawData := fmt.Sprintf("%d\n%s", len(respData), respData)
+	rawData := fmt.Sprintf("0\n%d\n%s", len(respData), respData)
 	return rawData
 }
